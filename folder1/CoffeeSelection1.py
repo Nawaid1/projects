0cdbe83c5a953df316daabc4bce888561558b3e9
Nawaid1 <86737055+Nawaid1@users.noreply.github.com>
2021-08-08 21:49:16 +0400

    Add files via upload

---
+++ folder1/CoffeeSelection1.py
@@ -0,0 +1,49 @@
+# people = [
+#     ['Aima','Jailani','aima@gmail.com'],
+#     ['firstname','lastname','email'],
+#     ['firstname','lastname','email'],
+#     ['firstname','lastname','email'],    
+# ]
+
+# print(people[0][2])
+
+
+coffeeOptions = ['Latte', 'Americano', 'Cappuccino', 'Espresso', 'Turkish']
+price = [23, 18, 19, 17, 20]
+
+a = 'Latte'
+
+print (coffeeOptions)
+print (price)
+
+a = input("which Coffee flavour would you like to drink? ")
+
+if a == 'Latte':
+    print("Latte Price is 23aed")
+else:
+    print("Please select correct coffee option")
+
+    
+
+  #  For loop
+# customers = [
+#     'Aima Grace',           
+#     'Corina Matthews', 
+#     'Deeksha Gupta', 
+#     'Lexie Vacca',
+#     'Lily Anethang'
+# ]
+
+# Using for loop with specified range
+# for index in range(0, 5):
+#     print(customers[index])
+
+# Using for loop with 'in'
+# for currentPerson in customers:
+#     print("Dear {},".format(currentPerson))
+#     print("You have been randomly selected for a prize")
+#     print("Click this link to claim your prize!")
+#     print("DON'T IGNORE OR YOUR PRIZE WILL EXPIRE")
+#     print("Regards,\nLegitimate Inc.\n\n")
+
+
